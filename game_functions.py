7ea9e7a5538de6114d25c06574b6b431590a2d60
e <2799594563@qq.com>
2020-02-16 15:12:15 +0800

    refactor File game_function/check_funtions

--- game_functions.py
+++ game_functions.py
@@ -1,5 +1,24 @@
 import sys
 import pygame
+
+def check_keydown_events(event, ship):
+    """响应按键"""
+    if event.key == pygame.K_RIGHT:
+        # 向右移动飞船
+        print("You press the k_right")
+        ship.moving_right = True
+    if event.key == pygame.K_LEFT:
+        # 向左移动飞船
+        print("You press the k_left")
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    """响应松开"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    if event.key == pygame.K_LEFT:
+        ship.moving_left = False 
+
 def check_events(ship):
     """响应按键和鼠标事件"""
     for event in pygame.event.get():
@@ -8,20 +27,10 @@ def check_events(ship):
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                # 向右移动飞船
-                print("You press the k_right")
-                ship.moving_right = True
-            if event.key == pygame.K_LEFT:
-                # 向左移动飞船
-                print("You press the k_left")
-                ship.moving_left = True
+            check_keydown_events(event, ship)
 
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            if event.key == pygame.K_LEFT:
-                ship.moving_left = False 
+            check_keyup_events(event, ship)
 
 def updata_screen(ai_setting, screen, ship):
     """更新屏幕上的图像，并切换到新的屏幕"""
