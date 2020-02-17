4f5b43c2c5910c57e8ab8b0a4f6f925fdb705599
e <2799594563@qq.com>
2020-02-17 13:50:46 +0800

    Many rows alien

--- game_functions.py
+++ game_functions.py
@@ -77,24 +77,43 @@ def update_bullets(bullets):
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
 
-def create_fleet(ai_settings, screen, aliens):
-    """创建外心人群"""
-    # 创建外星人群，并计算一行可容纳多少个外星人
-    # 外星人间距为外星人的宽度
-    alien = Alien(ai_settings, screen)
-    alien_width = alien.rect.width
+def get_number_alien_x(ai_settings, alien_width):
+    """计算每行可容纳多少个外星人"""
     available_space_x = ai_settings.screen_width - alien_width
     number_aliens_x = int(available_space_x / (2 * alien_width))
     print("The number of aliens each line:", number_aliens_x)
     print("Width:", alien_width)
-    
+    return number_aliens_x
+
+def get_number_rows(ai_settings, ship_height, alien_height):
+    """计算屏幕可容纳多少行外星人"""
+    available_space_y = (ai_settings.screen_height - (3 * alien_height) - ship_height)
+    number_rows = int(available_space_y / (2 * alien_height))
+    print("The number of row:", number_rows)
+    return number_rows
+
+def create_alien(ai_settings, screen, aliens, alien_number, row_number):
+    """创建一个外星然并将其放在当前行"""
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    alien.f_x = alien_width + 2 * alien_width * alien_number
+    alien.rect.x = alien.f_x
+    alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+    aliens.add(alien)
+    print("NO.", alien_number, "alien position:", (alien.rect.x, alien.rect.y))
+
+def create_fleet(ai_settings, screen, ship, aliens):
+    """创建外星人群"""
+    # 创建外星人群，并计算一行可容纳多少个外星人
+    # 外星人间距为外星人的宽度
+    alien = Alien(ai_settings, screen)
+    number_aliens_x = get_number_alien_x(ai_settings, alien.rect.width)
+    number_rows = get_number_rows(ai_settings, ship.rect.height, alien.rect.height)
+
     # 创建第一行外星人
-    for alien_number in range(number_aliens_x):
-        # 创建一个外星人并将其加入当前行
-        alien = Alien(ai_settings, screen)
-        alien.f_x = alien_width + 2 * alien_width * alien_number
-        alien.rect.x = alien.f_x
-        aliens.add(alien)
-        print("Number", alien_number, "position:", alien.f_x)
+    for row_number in range(number_rows):
+        for alien_number in range(number_aliens_x):
+            create_alien(ai_settings, screen, aliens, alien_number, row_number)
+         
 
 
